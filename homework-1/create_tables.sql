f2a013cb8f91908e43f4f2a793fb833cdff0d551
MariaSvintsova <m.svintsova@it-park.tech>
2023-11-30 15:03:27 +0300

    1 HW

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,36 @@
 -- SQL-команды для создания таблиц
+-- DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS orders CASCADE;
+DROP TABLE IF EXISTS customers CASCADE;
+DROP TABLE IF EXISTS employees CASCADE;
+
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name text,
+	last_name text,
+	title varchar(250) NOT NULL,
+	birth_date text,
+	notes text
+);
+SELECT * FROM employees;
+
+
+
+CREATE TABLE IF NOT EXISTS customers
+(
+	customer_id TEXT PRIMARY KEY,
+	company_name VARCHAR(250) NOT NULL,
+	contact_name VARCHAR(250) NOT NULL
+);
+SELECT * FROM customers;
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id text REFERENCES customers(customer_id) NOT NULL,
+	employee_id int REFERENCES employees(employee_id) NOT NULL,
+	order_date varchar(250) NOT NULL,
+	ship_city text
+);
+SELECT * FROM orders;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,47 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+
+import psycopg2
+
+
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='Msssvintsova006')
+
+try:
+    with conn.cursor() as cur:
+        with open('/Users/miya/PycharmProjects/postgres_homeworks/homework-1/north_data/customers_data.csv', 'r', encoding='utf-8') as file:
+            csv_reader = csv.reader(file)
+            headers = next(csv_reader, None)
+            for row in csv_reader:
+                cur.execute("INSERT INTO customers  (customer_id, company_name, contact_name) VALUES  (%s, %s, %s)", (row[0], row[1], row[2]))
+
+        cur.execute("SELECT * FROM customers")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+
+    with conn.cursor() as cur:
+        with open('/Users/miya/PycharmProjects/postgres_homeworks/homework-1/north_data/employees_data.csv', 'r', encoding='utf-8') as file:
+            csv_reader = csv.reader(file)
+            headers = next(csv_reader, None)
+            for row in csv_reader:
+                cur.execute("INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) VALUES  (%s, %s, %s, %s, %s, %s)", (row[0], row[1], row[2], row[3], row[4], row[5]))
+
+        cur.execute("SELECT * FROM employees")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+
+    with conn.cursor() as cur:
+        with open('/Users/miya/PycharmProjects/postgres_homeworks/homework-1/north_data/orders_data.csv', 'r', encoding='utf-8') as file:
+            csv_reader = csv.reader(file)
+            headers = next(csv_reader, None)
+            for row in csv_reader:
+                cur.execute("INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES  (%s, %s, %s, %s, %s)", (row[0], row[1], row[2], row[3], row[4]))
+
+        cur.execute("SELECT * FROM orders")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+finally:
+    conn.commit()
+    conn.close()
